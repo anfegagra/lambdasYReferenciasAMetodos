fffa157c8e79438c62130f1abf65f58f42280896
Andres Felipe García Granados <garandre@colpatria.com>
2019-08-25 16:30:38 -0500

    empezando ejemplo con referencia a métodos utilizando lambdas

---
+++ src/main/java/Trabajo.java
@@ -0,0 +1,7 @@
+package main.java;
+
+public interface Trabajo {
+
+	void hacerAlgo();
+
+}
---
+++ src/main/java/Usuario.java
@@ -0,0 +1,22 @@
+package main.java;
+
+public class Usuario {
+
+	private String nombre;
+
+	public Usuario(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public static void obtenerReferenciaAMetodoEstatico() {
+		System.out.println("Probando referencia a método estático");
+	}
+
+	public void ObtenerReferenciaAMetodoParticular() {
+		System.out.println("Probando referencia a método de objeto particular");
+	}
+
+	public void mostrarNombre() {
+		System.out.println(nombre);
+	}
+}
