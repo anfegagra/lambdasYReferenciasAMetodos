2b0fe2d2c7be0acddf039abe7a0c4aac0c3a3dcc
Andres Felipe García Granados <garandre@colpatria.com>
2019-09-10 21:44:48 -0500

    ejemplo de referencia a método estático

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,19 @@ package main.java;
 
 public class Main {
 
-	public static void main(String[] args) {
-		
-	}
+    public static void main(String[] args) {
+
+        // Referencia a static method Class::staticMethod Math::abstracto n ->Math.abs(n)
+
+        Trabajo trabajo = new Trabajo() {
+            @Override
+            public void hacerAlgo() {
+                Usuario.obtenerReferenciaAMetodoEstatico();
+            }
+        };
+
+        Trabajo trabajoLambda = () -> Usuario.obtenerReferenciaAMetodoEstatico();
+        Trabajo trabajoMetodoReferencia = Usuario::obtenerReferenciaAMetodoEstatico;
+        trabajoMetodoReferencia.hacerAlgo();
+    }
 }
